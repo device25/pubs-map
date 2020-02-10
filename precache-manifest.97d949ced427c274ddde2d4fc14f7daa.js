40cf7e40bb1331f8a93da76669b9cd2f99c886b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-10 20:59:15 +0000

    Deploy device25/pubs-map to github.com/device25/pubs-map.git:gh-pages

---
+++ precache-manifest.97d949ced427c274ddde2d4fc14f7daa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bca2ce1845f41bc3190a3beebb67488",
+    "url": "/pubs-map/index.html"
+  },
+  {
+    "revision": "e68cf7f97f41060b6085",
+    "url": "/pubs-map/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "b27cb822ee7b13367fc8",
+    "url": "/pubs-map/static/css/main.10ed7a93.chunk.css"
+  },
+  {
+    "revision": "e68cf7f97f41060b6085",
+    "url": "/pubs-map/static/js/2.88a34fc7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pubs-map/static/js/2.88a34fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27cb822ee7b13367fc8",
+    "url": "/pubs-map/static/js/main.f9975585.chunk.js"
+  },
+  {
+    "revision": "5f1e88c4cb2a090f0bd7",
+    "url": "/pubs-map/static/js/runtime-main.bfb43426.js"
+  }
+]);
