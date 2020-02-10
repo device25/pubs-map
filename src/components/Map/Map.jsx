e4250d68ba9e822e2437f904b1c69610aab0fa36
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2020-02-10 23:05:02 +0300

    feat(map): remove heatmap
    BREAKING CHANGE: no more heatmap

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,35 +8,27 @@ import mapToken from '../../config/mapToken';
 // utils
 import { pixelValue } from '../../utils';
 
-// styled
-import Container from './styled/Container';
+import Container from './Container';
 
 class Map extends PureComponent {
+  popup = new mapboxgl.Popup();
+
   componentDidMount() {
+    const { center } = this.props;
     mapboxgl.accessToken = mapToken;
     this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/light-v9',
-      center: this.props.center,
+      center,
       zoom: 9,
       minZoom: 8
-      // style: 'mapbox://styles/device25/ciutqc2xo01152jl8n8vgjkna',
     });
 
     this.map.on('load', this.onLoad);
   }
 
-  // componentWillReceiveProps(nextProps) {
-  //   const { filteredPubs } = nextProps;
-  //   const source = this.map.getSource('pubs');
-  //
-  //   if (source) {
-  //     source.setData(filteredPubs);
-  //   }
-  // }
-
   onLoad = () => {
-    const { center, accuracy } = this.props;
+    const { center, accuracy, pubs } = this.props;
 
     this.map.addSource('location', {
       type: 'geojson',
@@ -48,52 +40,15 @@ class Map extends PureComponent {
             properties: {},
             geometry: {
               type: 'Point',
-              coordinates: this.props.center
+              coordinates: center
             }
           }
         ]
       }
     });
-    this.map.addSource('earthquakes', {
-      type: 'geojson',
-      // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-      // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-      data: this.props.pubs,
-      cluster: true,
-      clusterMaxZoom: 20, // Max zoom to cluster points on
-      clusterRadius: 160 // Use small cluster radius for the heatmap look
-    });
     this.map.addSource('pubs', {
       type: 'geojson',
-      data: this.props.pubs
-    });
-
-    // Use the earthquakes source to create four layers:
-    // three for each cluster category, and one for unclustered points
-
-    // Each point range gets a different fill color.
-    const layers = [
-      [0, 'green'],
-      [20, 'orange'],
-      [200, 'red']
-    ];
-    layers.forEach((layer, i) => {
-      this.map.addLayer({
-        id: `cluster-${i}`,
-        type: 'circle',
-        source: 'earthquakes',
-        paint: {
-          'circle-color': layer[1],
-          'circle-radius': 70,
-          'circle-blur': 1 // blur the circles to get a heatmap look
-        },
-        filter: i === layers.length - 1
-          ? ['>=', 'point_count', layer[0]]
-          : ['all',
-            ['>=', 'point_count', layer[0]],
-            ['<', 'point_count', layers[i + 1][0]]
-          ]
-      });
+      data: pubs
     });
 
     this.map.addLayer({
@@ -152,9 +107,10 @@ class Map extends PureComponent {
   }
 
   onMouseMove = (e) => {
-    const features = this.map.queryRenderedFeatures(e.point, { layers: ['pubs'] });
+    const features = this.map.queryRenderedFeatures(
+      e.point, { layers: ['pubs'] }
+    );
     if (features.length > 0) {
-      // console.log(features[0]);
       const { properties } = features[0];
 
       this.popup
@@ -168,16 +124,15 @@ class Map extends PureComponent {
     }
   }
 
-  popup = new mapboxgl.Popup();
-
   render() {
-    return <Container id='map' />;
+    return (
+      <Container id='map' />
+    );
   }
 }
 
 Map.propTypes = {
   pubs: PropTypes.object.isRequired,
-  // filteredPubs: PropTypes.object.isRequired,
   center: PropTypes.arrayOf(PropTypes.number).isRequired,
   accuracy: PropTypes.number.isRequired
 };
